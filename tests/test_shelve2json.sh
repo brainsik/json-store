72c4d4fd14941f0803398d5f45cfddbbf59f122e
jeremy avnet <brainsik@users.noreply.github.com>
2022-08-04 23:39:48 -0700

    Avoid "db type could not be determined" error by deleting the tmpfile

--- tests/test_shelve2json.sh
+++ tests/test_shelve2json.sh
@@ -2,6 +2,7 @@
 set -e -x
 
 tmp=$(mktemp)
+rm -f "$tmp"
 python -c 'import shelve; db=shelve.open("'"$tmp"'", flag="n"); db["eggs"] = "eggs"; db.close()'
 shelve2json "$tmp"
 rm -f "$tmp" "$tmp.json"
