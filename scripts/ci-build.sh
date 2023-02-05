f53a9640ee658456cde3b7d9a2502040a0b99f50
jeremy avnet <brainsik@users.noreply.github.com>
2023-02-05 12:59:05 -0800

    Experiment with a reproducible build

---
+++ scripts/ci-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+if [ -z "${CI:-}" ]; then
+	echo "This should only run under CI."
+	exit 1
+fi
+
+# set all files to the VERSION timestamp
+find -E . -type f -regex '\./[^.].+' | xargs touch -r VERSION
+
+# set system date/time to the VERSION timestamp
+timedatectl set-time $(stat --format='%y' this | grep -oP '\d\d\d\d-\d\d-\d\d')
+timedatectl set-time $(stat --format='%y' this | grep -oP '\d\d:\d\d:\d\d')
+
+python3 -m build
